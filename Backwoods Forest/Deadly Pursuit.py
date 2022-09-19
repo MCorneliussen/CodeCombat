c310d6ef2c05c6b28b4663d378198bcc9c425ce1
CopyrightNOR <80279903+CopyrightNOR@users.noreply.github.com>
2022-09-19 15:43:26 +0200

    Create Deadly Pursuit.py

---
+++ Backwoods Forest/Deadly Pursuit.py
@@ -0,0 +1,13 @@
+# Grab all the coins and use flags to build traps behind
+# you to deal with those ogres.
+
+while True:
+    flag = hero.findFlag()
+    item = hero.findNearestItem()
+    if flag:
+        hero.buildXY("fire-trap", flag.pos.x, flag.pos.y)
+        hero.pickUpFlag(flag)
+        pass
+    elif item:
+        hero.moveXY(item.pos.x, item.pos.y)
+        pass
